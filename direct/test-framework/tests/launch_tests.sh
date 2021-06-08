eea0b87b01437ed6db77e796c85d496ed94b3570
Charles Manning <cdhmanning@gmail.com>
2021-06-08 15:46:55 +1200

    Make the tmpfs test space larger for running long tests
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>

--- direct/test-framework/tests/launch_tests.sh
+++ direct/test-framework/tests/launch_tests.sh
@@ -11,8 +11,10 @@ LAUNCHDIR=`pwd`
 RUNDIR=`pwd`/tmp
 mkdir $RUNDIR
 
+# Check if RUNDIR is mounted, if not, mount as tmpfs
+# because we don't want to hammer the disk.
 if [ -z "$(mount | grep $RUNDIR)" ]; then
-sudo mount -t tmpfs -osize=2G none $RUNDIR
+sudo mount -t tmpfs -osize=7G none $RUNDIR
 sudo chmod a+wr $RUNDIR
 fi
 
@@ -30,3 +32,4 @@ xterm  -e "$LAUNCHDIR/manage_m18_test.sh  2 $iterations"&
 xterm  -e "$LAUNCHDIR/manage_nand_test.sh  0 $iterations"&
 xterm  -e "$LAUNCHDIR/manage_nand_test.sh  1 $iterations"&
 xterm  -e "$LAUNCHDIR/manage_nand_test.sh  2 $iterations"&
+xterm  -e "$LAUNCHDIR/manage_nand_test.sh  3 $iterations"&
